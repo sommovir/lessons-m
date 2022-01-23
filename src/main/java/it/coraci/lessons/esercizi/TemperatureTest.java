46f2640299dbcccc53cf4867e93a4d9b95cf79eb
Marica <ceccarino.marica@libero.it>
2022-01-23 17:11:38 +0100

    esercizi array

--- src/main/java/it/coraci/lessons/esercizi/TemperatureTest.java
+++ src/main/java/it/coraci/lessons/esercizi/TemperatureTest.java
@@ -9,26 +9,58 @@ package it.coraci.lessons.esercizi;
  * @author sommovir
  */
 public class TemperatureTest {
-    
+
     //creare un metodo che ci dica se l'array contiene temperature inferiori allo 0. 
-    
-    public static boolean containsNegativeTemps(int [] a){
+    public static boolean containsNegativeTemps(int[] a) {
         for (int i = 0; i < a.length; i++) {
-            if(a[i] < 0){
+            if (a[i] < 0) {
                 return true;
             }
         }
         return false;
     }
-    
-    
-    public static void main(String[] args) {
-        
-        int [] temps = new int[]{10,15,34,34,87,22,11,18,25,15,-1,22};
-        
-        
-        
-        
+    //creare un metodo che restituisce il numero di temperature superiori a x ,
+    //dove x è passato in argomento
+
+    public static int countTemperatures(int[] t, int x) {
+        int c = 0;
+        for (int i = 0; i < t.length; i++) {
+            if (t[i] > x) {
+                c++;
+            }
+
+        }
+        return c;
+    }
+
+    //creare un metodo che restituisca la temperatura massima dell'array
+    public static int maxTemperature (int[]t){
+        int max = t[0];
+        for (int i = 1; i< t.length; i++){
+            if (t[i] > max){
+                max=t[i];
+            }
+                   
+        }
+        return max;
     }
     
+    public static void main(String[] args) {
+
+        int[] temps = new int[]{10, 15, 34, 34, 87, 22, 11, 18, 25, 15, -1, 22};
+
+        boolean result = containsNegativeTemps(temps);
+        int result2 = countTemperatures(temps, 25);
+        int max = maxTemperature(temps);
+        System.out.println("l'array contiene un numero di valori superiore a 25 pari a:" + result2);
+        if (result == true) {
+            System.out.println("l'array contiene numeri negativi");
+        }
+        else {
+            System.out.println("l'array non contiene numeri negativi");
+            
+        }
+        System.out.println("la massima temperatura è:"+max);
+    }
+
 }
